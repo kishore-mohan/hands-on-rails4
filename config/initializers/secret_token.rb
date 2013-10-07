6c97045174b2bff249b285775f17f244f7013d0f
kishore-mohan <kishorevlrtn@gmail.com>
2013-10-07 21:19:24 +0530

    Initializing Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HandsOnRails4::Application.config.secret_key_base = '416b0fef85d80dfa2091eccafe0af33b6bd8653bb3c389403dad9d6b53cbdee70561edfdbdf20a5a13435a8f08f0b97fa27f10ac26fbc6ce840196c98f04e05c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HandsOnRails4::Application.config.session_store :cookie_store, key: '_hands_on_rails4_session'
